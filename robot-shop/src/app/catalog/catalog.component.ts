7a225c40559d7dae2356691c836910b30cfc11ea
saicharan293 <2493saicharan@gmail.com>
2024-06-24 11:39:28 +0530

    binding component data with interpolation

--- robot-shop/src/app/catalog/catalog.component.ts
+++ robot-shop/src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'app-catalog',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
+  product: IProduct;
 
+  constructor(){
+    this.product={
+      id:2,
+      description:"A friendly robot with two eyes with a smile -- great for domestic use",
+      name: 'Friendly robot',
+      imageName:'head-friendly.png',
+      category:'Heads',
+      price: 945.0,
+      discount: 0.2
+    };
+  }
 }
