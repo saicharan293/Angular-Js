a570f9ac749813d75e077247b791edcb5ca17fa2
saicharan293 <2493saicharan@gmail.com>
2024-07-01 11:39:42 +0530

    sign in form is added along with route set up in site header component

--- robot-shop/src/app/app-routing.module.ts
+++ robot-shop/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 const routes:Routes=[
   {path:'home',component:HomeComponent,title:"Home - Joe's Robot shop"},
   {path:'catalog',component:CatalogComponent,title:"Catalog - Joe's Robot shop"},
   {path:'cart',component:CartComponent,title:"Cart - Joe's Robot shop"},
+  {path:'sign-in',component:SignInComponent,title:'sign-in'},
   {path:'',redirectTo:'/home',pathMatch:"full"},
 ];
 
