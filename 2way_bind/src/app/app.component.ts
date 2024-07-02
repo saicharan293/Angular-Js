ba251f1e245c37d205481ade8cee9d1bf4076cae
saicharan293 <2493saicharan@gmail.com>
2024-07-02 16:36:19 +0530

    2-way data bining is applied on button along with toggle(switch) values on click event

--- 2way_bind/src/app/app.component.ts
+++ 2way_bind/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = '2way_bind';
   city:string='kurnool';
+
+  updateCity(){
+    this.city=='kurnool'?(this.city='guntakal'):(this.city='kurnool');
+  }
 }
