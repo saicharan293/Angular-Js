232500eb2ad0f655104bb99e7029605a4e32f668
saicharan293 <2493saicharan@gmail.com>
2024-07-02 17:11:53 +0530

    directives started to switch between places and also to decide whether to show or hide the city place

--- 2way_bind/src/app/app.component.ts
+++ 2way_bind/src/app/app.component.ts
@@ -8,7 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = '2way_bind';
   city:string='kurnool';
+  show_city:boolean=false;
+  buttonText: string = 'show';
 
+  displayCity(){
+    this.show_city==false?(this.show_city=true):(this.show_city=false);
+    this.buttonText=this.show_city?'hide':'show'
+  }
   updateCity(){
     this.city=='kurnool'?(this.city='guntakal'):(this.city='kurnool');
   }
