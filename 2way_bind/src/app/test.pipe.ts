6b576fc0bb2a060d902677c77b412de7e376b806
saicharan293 <2493saicharan@gmail.com>
2024-07-05 14:45:57 +0530

    extra params to the custom pipe test

--- 2way_bind/src/app/test.pipe.ts
+++ 2way_bind/src/app/test.pipe.ts
@@ -7,6 +7,7 @@ export class TestPipe implements PipeTransform {
 
   transform(value: unknown, ...args: unknown[]): unknown {
     // value is a variiable that stores the value of strings in html 
+    console.log(args);
     console.log('company name is: ',value);
     return "company name is confidential";
   }
