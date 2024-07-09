92810ddeee1614ff21de761c22295d40ec340187
saicharan293 <2493saicharan@gmail.com>
2024-07-09 12:13:36 +0530

    routing is done using router-outlet, routerModule, routerLink, routerLinkActive

--- 2way_bind/src/app/app.component.ts
+++ 2way_bind/src/app/app.component.ts
@@ -7,29 +7,4 @@ import {Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angul
 export class AppComponent { // Implement AfterViewInit
   title = '2way_bind';
 
-  // Added type annotation
-  @ViewChild('para') p!: ElementRef; 
-  @ViewChildren('heading') h!: QueryList<ElementRef>;
-  test(){
-    //to access first p having para as ref variable
-    console.log(this.p.nativeElement.innerText);
-
-    //to access first h element with heading as ref variable
-    console.log(this.h.first.nativeElement.innerText);
-
-    //to access last h element with heading as ref variable
-    console.log(this.h.last.nativeElement.innerText);
-
-    // to access the array of h elements having heading as ref variable
-    // using .toArray()
-    const headingArray = this.h.toArray();
-    headingArray.forEach(ele=>console.log(ele.nativeElement.innerText))
-
-    // using forEach method on the object directly
-    // this.h.forEach((ele) => {
-    //   console.log(ele.nativeElement.innerText);
-    // });
-  }
-
-
 }
--- 2way_bind/src/app/app.module.ts
+++ 2way_bind/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,17 +6,42 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
 import { TestPipe } from './test.pipe';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule,Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const routes:Routes=[
+  {
+    path:'',component:HomeComponent
+  },
+  {
+    path:'about',component:AboutComponent
+  },
+  {
+    path:'contact',component:ContactComponent
+  },
+  {
+    path:"**",component:NotfoundComponent
+  }
+  
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TestPipe
+    TestPipe,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
