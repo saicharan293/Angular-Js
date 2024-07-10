db6ec6d7f208d305e8d9154e4300dc0766564910
saicharan293 <2493saicharan@gmail.com>
2024-07-10 17:08:00 +0530

    observables are added along with subscribe method

--- 2way_bind/src/app/app.component.ts
+++ 2way_bind/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,5 +10,19 @@ export class AppComponent {
   // Implement AfterViewInit
   title = '2way_bind';
   constructor(private http: HttpClient) {}
-  
+  myobs = new Observable((listener) => {
+    listener.next(1);
+    listener.next(2);
+    setTimeout(() => {listener.next(3);}, 1000);
+    setTimeout(() => {listener.next(4);}, 1000);
+    setTimeout(() => {listener.next(6);}, 1000);
+    setTimeout(() => {listener.complete();}, 1000);
+  });
+  subs(){
+    this.myobs.subscribe(
+      data=>console.log(data),
+      err=>console.log('error',err),
+      ()=>console.log("completed")
+    )
+  }
 }
