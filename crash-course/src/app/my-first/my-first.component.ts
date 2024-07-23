2980e1112576d2626b0a5cc98857b2ca8822c8ed
saicharan293 <2493saicharan@gmail.com>
2024-07-23 18:47:06 +0530

    instead of direct functionality, services are introduced for loosely coupling

--- crash-course/src/app/my-first/my-first.component.ts
+++ crash-course/src/app/my-first/my-first.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MyFirstService } from '../services/my-first.service';
 
 @Component({
   selector: 'app-my-first',
@@ -13,9 +14,14 @@ export class MyFirstComponent {
   message:string='this is first message';
   isSubmitted:boolean=true;
   messages:Array<any>=[];
+
+  constructor(private service:MyFirstService){
+    this.messages=this.service.getAll();
+    this.isSubmitted=this.messages.length > 0;
+  }
   onSubmit() {
     this.isSubmitted=true;
-    this.messages.push({
+    this.service.insert({
       'name':this.myname,
       'email':this.email,
       'message':this.message
@@ -26,7 +32,7 @@ export class MyFirstComponent {
     
   }
   deleteMessage(index: number) {
-    this.messages.splice(index,1);
+    this.service.deleteMessage(index);
   }
   
 }
---
+++ crash-course/src/app/services/my-first.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyFirstService {
+
+  messages:Array<any>=[];
+  constructor() { 
+    this.init();
+  }
+
+  init():void{
+    this.insert({
+      name:'sai',
+      email:'sai@gmial.com',
+      message:'this is sai'
+    });
+    this.insert({
+      name:'shiva',
+      email:'shiva@gmial.com',
+      message:'this is shiva'
+    });
+    this.insert({
+      name:'charan',
+      email:'charan@gmial.com',
+      message:'this is charan'
+    });
+  }
+
+  insert(message:{name:string,email:string,message:string}):void{
+    this.messages.push(message)
+  }
+  getAll(){
+    return this.messages;
+  }
+  deleteMessage(index:number):void{
+    this.messages.splice(index,1)
+  }
+}
