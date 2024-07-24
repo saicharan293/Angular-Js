ecfea02169cbfd711d664548214cfff5e8fe164c
saicharan293 <2493saicharan@gmail.com>
2024-07-24 12:35:36 +0530

    Programmatic implementation of routing for about page

--- crash-course/src/app/menu/menu.component.ts
+++ crash-course/src/app/menu/menu.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
 
+
+  //programmatic implementation of routing for about page using Router module
+  constructor(private router:Router){}
+  navigateToAbout() {
+    this.router.navigate(['about'])
+  }
 }
