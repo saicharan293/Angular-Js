25a0eb48e6bdc1c9db265ef8e1ac95c0902bcf51
saicharan293 <2493saicharan@gmail.com>
2024-07-24 12:48:54 +0530

    params(/:) and query params(?) are introduced

--- crash-course/src/app/about/about.component.ts
+++ crash-course/src/app/about/about.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  param:any;
+  queryparam_course:any;
+  queryparam_lname:any;
+  constructor(private activatedRoute: ActivatedRoute){
 
+  }
+  ngOnInit(): void {
+    console.log(this.activatedRoute)
+    this.param=this.activatedRoute.snapshot.params['username']
+    this.queryparam_course=this.activatedRoute.snapshot.queryParams['course']
+    this.queryparam_lname=this.activatedRoute.snapshot.queryParams['lastname']
+  }
 }
