4d7a4391a367ec151cccfc4b492e2db8d26bb2f3
saicharan293 <2493saicharan@gmail.com>
2024-07-25 19:03:54 +0530

    todos model is created

--- course-with-Harry/src/app/MyComponents/todos/todos.component.ts
+++ course-with-Harry/src/app/MyComponents/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from '../../Todos';
 
 @Component({
   selector: 'app-todos',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './todos.component.css'
 })
 export class TodosComponent {
-
+  todos:Todo[]=[];
+  constructor(){
+    this.todos=[
+      {
+        sno:1,
+        title:"this is title",
+        description:"description",
+        active:true
+      },
+      {
+        sno:2,
+        title:"this is title2",
+        description:"description",
+        active:true
+      },
+      {
+        sno:3,
+        title:"this is title3",
+        description:"description",
+        active:true
+      },
+    ]
+  }
 }
