5e84a2e47b6a0761269cfe2a1cb993175e793eae
saicharan293 <2493saicharan@gmail.com>
2024-07-25 15:18:23 +0530

    use of query params and params is added

--- crash-course-2/src/app/about/about.component.ts
+++ crash-course-2/src/app/about/about.component.ts
@@ -11,8 +11,12 @@ export class AboutComponent implements OnInit {
 
   }
   param:any;
+  lname:any;
+  course:any;
   ngOnInit(): void {
     // console.log(this.activatedRoute);
     this.param=this.activatedRoute.snapshot.params['username'];
+    this.lname=this.activatedRoute.snapshot.queryParams['lname'];
+    this.course=this.activatedRoute.snapshot.queryParams['course'];
   }
 }
