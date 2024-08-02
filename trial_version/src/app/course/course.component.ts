79a77b6ae30b65a5835c17c1950269bc483f9688
saicharan293 <2493saicharan@gmail.com>
2024-08-02 19:07:02 +0530

    done upto structural directive (*ngFor)

--- trial_version/src/app/course/course.component.ts
+++ trial_version/src/app/course/course.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
 
+interface Courses{
+  name:string,
+  img:string,
+  content:string,
+  duration:number,
+  fee:number
+}
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrl: './course.component.css'
 })
 export class CourseComponent {
-
+  name:string='Course List'
+  courseList:Courses[]=[
+    {
+      name:'Angular',
+      img:'first.png',
+      content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, dignissimos?',
+      duration:60,
+      fee:15000
+    },
+    {
+      name:'Node js',
+      img:'second.jpeg',
+      content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, dignissimos?',
+      duration:45,
+      fee:15000
+    },
+    {
+      name:'Git and Git hub',
+      img:'third.jpeg',
+      content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, dignissimos?',
+      duration:30,
+      fee:15000
+    },
+    {
+      name:'MongoDB',
+      img:'fourth.png',
+      content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, dignissimos?',
+      duration:30,
+      fee:15000
+    },
+    {
+      name:'E js',
+      img:'fifth.jpeg',
+      content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, dignissimos?',
+      duration:30,
+      fee:15000
+    },
+    {
+      name:'Express js',
+      img:'sixth.png',
+      content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, dignissimos?',
+      duration:30,
+      fee:15000
+    },
+  ]
 }
--- trial_version/src/app/home/home.component.ts
+++ trial_version/src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public year = 2000;
 
+  ngOnInit(): void {
+    console.log(this.getAge())
+  }
+
+  getAge(){
+    let yr=new Date().getFullYear()
+    return yr-this.year
+  }
 }
